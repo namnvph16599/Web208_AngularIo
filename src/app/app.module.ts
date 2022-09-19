f6f3dc7fb6fc8fcf58435b1b77850e483c2005e8
Ngô Vưn Nam <anh02284@gmail.com>
2022-09-19 14:24:58 +0700

    lab2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductsComponent } from './products/products.component';
 import { Lab1Component } from './lab/lab1/lab1.component';
+import { HeaderComponent } from './lab/lab2/header/header.component';
+import { ProductlistComponent } from './lab/lab2/productlist/productlist.component';
 @NgModule({
   declarations: [
     AppComponent,
     ProductDetailComponent,
     ProductsComponent,
     Lab1Component,
+    HeaderComponent,
+    ProductlistComponent,
 
   ],
   imports: [
